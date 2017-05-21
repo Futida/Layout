e34d1b649b73c2f89d4dd92c782389dd984baf32
Konstantin <mihrutka@inbox.ru>
2017-05-21 13:45:21 +0300

    commit

--- js/module.js
+++ js/module.js
@@ -1,23 +1,22 @@
 var infograf = document.querySelectorAll(".infograf")
 
 
-	for (var i = 0; i < 43; i++){
-		var el = infograf[i];
-		var height = [];
+for (var i = 0; i < 43; i++) {
+	var el = infograf[i];
+	var height = [];
 
-		for (var k = 0; k < 12 ; k++){
-			var random = Math.floor((Math.random() * 100));
-			height.push(random)
-				}
-
-				for (var j = 0; j < height.length; j++) {
-					var h = height[j];
-					var span = document.createElement("span");
-					span.classList.add("infograf-item");
-					console.log(j)
-					if (j == height.length-1)
-						span.classList.add("black");
-					span.style.height = h + '%';
-					el.appendChild(span);
-		}
+	for (var k = 0; k < 12; k++) {
+		var random = Math.floor((Math.random() * 10));
+		height.push(random)
 	}
+
+	for (var j = 0; j < height.length; j++) {
+		var h = height[j];
+		var span = document.createElement("span");
+		span.classList.add("infograf-item");
+		if (j == height.length - 1)//последний спан черный
+		span.classList.add("black");
+		span.style.height = h + 'px';
+		el.appendChild(span);
+	}
+}
